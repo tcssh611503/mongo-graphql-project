519e500cd21ee29acb42c11443fc7f23e68d8dc8
york <york@ruten.co.jp>
2021-03-17 11:42:42 +0800

    Hello GraphQL

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+ 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4010);
+console.log('Running a GraphQL API server at http://localhost:4010/graphql');
+
+//run
+//node server.js
+
+// URL
+// http://localhost:4010/graphql
